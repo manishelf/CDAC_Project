2aa7352b2c3eda88b15dedf6e339077b8365c7d2
Manish patil <manishpatil7058@gmail.com>
2025-01-29 19:12:10 +0530

    client ui google maps api search and places implemented

--- FrontEnd/clientside/parkngo_enduser/src/App.jsx
+++ FrontEnd/clientside/parkngo_enduser/src/App.jsx
@@ -3,14 +3,22 @@ import PaymentReciptScreen from './screens/PaymentReciptScreen';
 import 'react-toastify/dist/ReactToastify.css';
 import UserReviewScreen from './screens/UserReviewScreen';
 import PaymentConfirmationScreen from './screens/PaymentConfirmationScreen';
+import MapWithDirection from './components/MapWithDirection';
 
 function App() {
   return (
     <>
     {/* <UserReviewScreen />
-    <PaymentReciptScreen /> */}
+    <PaymentReciptScreen />
     <PaymentConfirmationScreen />
     <ToastContainer />
+	*/}
+	<div style={{height: '100vh', width: '100%'}}>
+      <MapWithDirection 
+      from='Neelaya society, Talegaon Dabhade, Pune, Maharashtra' 
+      to='Sunbeam info tech, Pune, Maharashtra'
+      />
+    </div>
     </>
   );
 }
