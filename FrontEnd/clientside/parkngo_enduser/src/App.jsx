0d32ff0e6343d38d0765a9fb0c120794f2fd2784
Manish patil <manishpatil7058@gmail.com>
2025-02-09 23:58:17 +0530

    corrections

--- FrontEnd/clientside/parkngo_enduser/src/App.jsx
+++ FrontEnd/clientside/parkngo_enduser/src/App.jsx
@@ -1,7 +1,7 @@
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './screens/auth/Login';
